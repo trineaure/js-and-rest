844443b70158be92083e855b2e9658f0b03ad91e
Trine Aure <trine.aure@hotmail.com>
2017-10-17 20:19:26 +0200

    fix

--- tvShow/tcShowRouter.js
+++ tvShow/tcShowRouter.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const TvShow = require('./TvShow');
 const tcShowRouter = express.Router();
-const tvShows = [];
 const tvShows = [new TvShow(1, 'Mr.Robot', 'Drama'), new TvShow(2, 'Black Mirror', 'Drama')];
 
-tcShowRouter.get('/', (req, res) => {
+tcShowRouter.get('/tvShows', (req, res) => {
   res.json(tvShows);
 });
 
