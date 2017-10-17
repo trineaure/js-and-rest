1f312eb09b08c458dbb83b0384e949bfdf034496
Trine Aure <trine.aure@hotmail.com>
2017-10-17 20:10:25 +0200

    some some

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const tvShowRouter = require('./tvShow/tvShowRouter');
+
 const app = express();
 const port = 3000;
-const morgan = require('morgan');
+
+app.use(bodyParser.json());
 app.use(morgan('dev')); 
 
+//Exercise #2
+app.use('/tvshow', tvShowRouter);
+
+//Exercise #1
 app.get('/', (request, response) => response.send('Hello World'));
 
 app.listen(port, () => {
