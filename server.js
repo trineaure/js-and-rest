11c6276da9b0e1c55e44c5fdea9f054307ac0ead
Trine Aure <trine.aure@hotmail.com>
2017-10-17 19:33:46 +0200

    Morgan

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const morgan = require('morgan');
+app.use(morgan('dev')); 
 
 app.get('/', (request, response) => response.send('Hello World'));
 
